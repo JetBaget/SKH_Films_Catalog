4c236803b8b07d74897bf7d7e12cceeefabcbe9f
JetBaget <jetneitrino@gmail.com>
2025-04-29 20:03:53 +0300

    File with films data

---
+++ api/api_v1/films/crud.py
@@ -0,0 +1,19 @@
+from schemas.film_info import FilmInfo
+
+
+FILMS = [
+    FilmInfo(
+        id=1,
+        genre="Fantasy",
+        name="Star Wars 1",
+        description="Princess Lea and Shubaca action",
+        age_restriction=10,
+    ),
+    FilmInfo(
+        id=3,
+        genre="Gachi",
+        name="Boss of the gym",
+        description="The struggle of men in oil for the right to be the best",
+        age_restriction=18,
+    ),
+]
