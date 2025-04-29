1a2ae81f1d631e8645176b48b091e811827088e9
JetBaget <jetneitrino@gmail.com>
2025-04-29 09:50:53 +0300

    New view 'hello' with doc link

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
+import uvicorn
+from dns.resolver import query
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 
-app = FastAPI()
+app = FastAPI(title="Film Catalog")
+
+
+@app.get("/")
+def hello(request: Request, name: str = "World"):
+    docs_url = request.url.replace(path="docs", query="")
+    return {"message": f"Hello, {name}!", "docs": str(docs_url)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
