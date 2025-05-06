a8480041469dc9ddc7a64c2941ca0533f53b9b1f
JetBaget <jetneitrino@gmail.com>
2025-05-06 10:38:13 +0300

    Post method for add a new film info

--- api/api_v1/films/views.py
+++ api/api_v1/films/views.py
@@ -1,8 +1,13 @@
+from random import randint
+
+from annotated_types import Len
 from typing import Annotated
 
 from fastapi import (
     Depends,
     APIRouter,
+    status,
+    Form,
 )
 
 from .crud import FILMS
@@ -20,6 +25,38 @@ router = APIRouter(
 @router.get(path="/", response_model=list[FilmInfo])
 def read_films_list():
     return FILMS
+
+
+@router.post(
+    path="/",
+    response_model=FilmInfo,
+    status_code=status.HTTP_201_CREATED,
+)
+def add_film(
+    name: Annotated[
+        str,
+        Len(min_length=3, max_length=20),
+        Form(),
+    ],
+    description: Annotated[
+        str,
+        Len(min_length=10, max_length=100),
+        Form(),
+    ],
+    genre: Annotated[
+        str,
+        Len(min_length=3, max_length=20),
+        Form(),
+    ],
+    age_restriction: Annotated[int, Form()] = 10,
+):
+    return FilmInfo(
+        id=randint(1, 100),
+        name=name,
+        description=description,
+        genre=genre,
+        age_restriction=age_restriction,
+    )
 
 
 @router.get(
