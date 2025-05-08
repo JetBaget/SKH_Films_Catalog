76fd15b38d46a296984e4bdfa5b96b74a54d4f3a
JetBaget <jetneitrino@gmail.com>
2025-05-08 16:56:02 +0300

    New schema for creating film info

--- schemas/film_info.py
+++ schemas/film_info.py
@@ -1,15 +1,26 @@
+from typing import Annotated
+
+from annotated_types import Len
 from pydantic import BaseModel
 
 
 class FilmInfoBase(BaseModel):
-    id: int
     name: str
     description: str
     genre: str
     age_restriction: int
 
 
+class FilmInfoCreate(FilmInfoBase):
+    description: Annotated[
+        str,
+        Len(min_length=10, max_length=25),
+    ]
+
+
 class FilmInfo(FilmInfoBase):
     """
     Модель описания фильма
     """
+
+    id: int
