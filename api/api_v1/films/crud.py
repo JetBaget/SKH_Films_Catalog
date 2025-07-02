52cc1d130327dc4138ecd6bc2fd3d93aab3cc8e9
JetBaget <jetneitrino@gmail.com>
2025-07-02 12:25:18 +0300

    Local storage for FilmsStorage model data

--- api/api_v1/films/crud.py
+++ api/api_v1/films/crud.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pydantic_core
 from pydantic import BaseModel
 
 from schemas.film_info import (
@@ -7,9 +10,27 @@ from schemas.film_info import (
     FilmInfoPartialUpdate,
 )
 
+JSON_DATA_PATH = "data.json"
+
 
 class FilmsStorage(BaseModel):
     slug_to_film: dict[str, FilmInfo] = {}
+
+    def load_from_json(self):
+        p = Path(JSON_DATA_PATH)
+        p.touch(exist_ok=True)
+        json_data = p.read_text()
+        if json_data:
+            try:
+                data = self.model_validate_json(json_data)
+                self.slug_to_film = data.slug_to_film
+            except pydantic_core.ValidationError as err:
+                print(f"Pydantic error: {err}")
+
+    def save_to_json(self):
+        data = self.model_dump_json()
+        p = Path(JSON_DATA_PATH)
+        p.write_text(data)
 
     def get(self) -> list[FilmInfo]:
         return list(self.slug_to_film.values())
@@ -22,13 +43,16 @@ class FilmsStorage(BaseModel):
             **film_info_in.model_dump(),
         )
         self.slug_to_film[film_info.slug] = film_info
+        self.save_to_json()
         return film_info
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_film.pop(slug, None)
+        self.save_to_json()
 
     def delete(self, film_info: FilmInfo) -> None:
         self.delete_by_slug(slug=film_info.slug)
+        self.save_to_json()
 
     def update(
         self,
@@ -37,6 +61,7 @@ class FilmsStorage(BaseModel):
     ) -> FilmInfo:
         for field_name, value in film_info_in:
             setattr(film_info, field_name, value)
+        self.save_to_json()
         return film_info
 
     def update_partial(
@@ -46,10 +71,12 @@ class FilmsStorage(BaseModel):
     ) -> FilmInfo:
         for field_name, value in film_info_in.model_dump(exclude_unset=True).items():
             setattr(film_info, field_name, value)
+        self.save_to_json()
         return film_info
 
 
 storage = FilmsStorage()
+storage.load_from_json()
 
 storage.create(
     FilmInfoCreate(
